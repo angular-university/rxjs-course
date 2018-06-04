05b41b2187452c2d3498384b02415ae6a40a71b4
JHades <jhades.dev@gmail.com>
2018-06-04 15:46:37 +0200

    RxJs Course

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,31 +1,38 @@
-
-
-import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Course} from "../model/course";
-import {Lesson} from "../model/lesson";
-import {map} from 'rxjs/operators';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Course} from '../model/course';
+import {Lesson} from '../model/lesson';
 import {API_URL} from '../common/constants';
-
 
 
 @Injectable()
 export class CoursesService {
 
-    constructor(private http: HttpClient) {
-
-    }
-
     findCourseById(courseId: string): Observable<Course> {
-        return this.http.get<Course>(`${API_URL}/courses/${courseId}.json`);
+        return this.httpToObservable(`${API_URL}/courses/${courseId}.json`);
     }
 
     findAllCourses(): Observable<Course[]> {
-        return this.http.get<Course[]>(`${API_URL}/courses.json`);
+        return this.httpToObservable(`${API_URL}/courses.json`);
     }
 
-    findAllCourseLessons(courseId:string): Observable<Lesson[]> {
-        return this.http.get<Lesson[]>(`${API_URL}/lessons/${courseId}.json`);
+    findAllCourseLessons(courseId: string): Observable<Lesson[]> {
+        return this.httpToObservable(`${API_URL}/lessons/${courseId}.json`);
     }
+
+    httpToObservable(url: string) {
+        return Observable.create(observer => {
+            fetch(url)
+                .then(response => response.json())
+                .then(body => {
+                    observer.next(body);
+                    observer.complete();
+                })
+                .catch(err => observer.error(err));
+        });
+    }
+
 }
+
+
