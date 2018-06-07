3b0ec4fda4cbb1398ea2176ed8b5bfc278198ad0
JHades <jhades.dev@gmail.com>
2018-06-07 14:00:32 +0200

    RxJs Course

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -74,3 +74,7 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Need to import at least one locale-data with intl.
  */
 // import 'intl/locale-data/jsonp/en';
+
+
+(window as any).global = window;
+
