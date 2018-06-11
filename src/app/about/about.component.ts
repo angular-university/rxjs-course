55f9f8225cd89a30455c71c21d61f62112cc2f14
Your Name <you@example.com>
2018-06-11 16:24:29 +0200

    RxJs Course

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,18 +1,37 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {fromEvent, interval, noop, observable, Observable, timer} from 'rxjs';
+import {delayWhen, map, timeout} from 'rxjs/operators';
+import {createHttpObservable} from '../common/util';
 
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+    selector: 'about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+    ngOnInit() {
 
-  ngOnInit() {
+        const http$ = createHttpObservable('/api/courses');
+
+        const courses$ = http$
+            .pipe(
+              map(res => Object.values(res["payload"]) )
+            );
 
 
+        courses$.subscribe(
+            courses => console.log(courses),
+            noop,
+            () => console.log('completed')
+        );
 
-  }
+    }
 
 }
+
+
+
+
+
+
--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,2 +1,28 @@
+import {Observable} from 'rxjs';
 
+
+export function createHttpObservable(url:string) {
+    return Observable.create(observer => {
+
+        fetch('/api/courses')
+            .then(response => {
+
+                return response.json();
+
+            })
+            .then(body => {
+
+                observer.next(body);
+
+                observer.complete();
+
+            })
+            .catch(err => {
+
+                observer.error(err);
+
+            })
+
+    });
+}
 
