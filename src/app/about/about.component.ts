3cc55289867825c4320e0db544c628d979dc3d8e
Your Name <you@example.com>
2018-06-12 12:18:16 +0200

    RxJs Course

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -12,19 +12,7 @@ export class AboutComponent implements OnInit {
 
     ngOnInit() {
 
-        const http$ = createHttpObservable('/api/courses');
 
-        const courses$ = http$
-            .pipe(
-              map(res => Object.values(res["payload"]) )
-            );
-
-
-        courses$.subscribe(
-            courses => console.log(courses),
-            noop,
-            () => console.log('completed')
-        );
 
     }
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
+import {interval, noop, Observable, of, timer} from 'rxjs';
 import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import {createHttpObservable} from '../common/util';
 
 
 @Component({
@@ -11,14 +12,36 @@ import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/oper
 })
 export class HomeComponent implements OnInit {
 
+    beginnerCourses$: Observable<Course[]>;
 
-    constructor() {
+    advancedCourses$: Observable<Course[]>;
 
-    }
 
     ngOnInit() {
 
+        const http$ : Observable<Course[]> =
+            createHttpObservable('/api/courses');
 
+        const courses$ = http$
+            .pipe(
+                tap(() => console.log("HTTP request executed")),
+                map(res => Object.values(res["payload"]) ),
+                shareReplay()
+            );
+
+        courses$.subscribe();
+
+        this.beginnerCourses$ = courses$
+            .pipe(
+                map(courses => courses
+                    .filter(course => course.category == 'BEGINNER'))
+            );
+
+        this.advancedCourses$ = courses$
+            .pipe(
+                map(courses => courses
+                    .filter(course => course.category == 'ADVANCED'))
+            );
 
     }
 
