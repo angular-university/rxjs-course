3b9514976c0d131b1720a7e388f6fa46de9ac1bd
Your Name <you@example.com>
2018-06-13 11:11:21 +0200

    RxJs Course

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -64,6 +64,11 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
 
     ngAfterViewInit() {
 
+        fromEvent(this.saveButton.nativeElement, 'click')
+            .pipe(
+                exhaustMap(() => this.saveCourse(this.form.value))
+            )
+            .subscribe();
 
     }
 
