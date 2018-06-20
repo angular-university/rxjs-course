7c8686a748fc396cb07e07720d8de77253bb1873
JHades <jhades.dev@gmail.com>
2018-06-20 10:53:17 +0200

    RxJs Course
    # Conflicts:
    #	src/app/about/about.component.css
    #	src/app/about/about.component.html

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -4,7 +4,7 @@ import {Course} from "../model/course";
 import {FormBuilder, Validators, FormGroup} from "@angular/forms";
 import * as moment from 'moment';
 import {fromEvent} from 'rxjs';
-import {concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap} from 'rxjs/operators';
+import {concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap, tap} from 'rxjs/operators';
 import {fromPromise} from 'rxjs/internal-compatibility';
 
 @Component({
@@ -12,7 +12,7 @@ import {fromPromise} from 'rxjs/internal-compatibility';
     templateUrl: './course-dialog.component.html',
     styleUrls: ['./course-dialog.component.css']
 })
-export class CourseDialogComponent implements OnInit, AfterViewInit {
+export class CourseDialogComponent implements AfterViewInit {
 
     form: FormGroup;
 
@@ -38,37 +38,9 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
 
     }
 
-    ngOnInit() {
-
-        this.form.valueChanges
-            .pipe(
-                filter(() => this.form.valid),
-                concatMap(changes => this.saveCourse(changes))
-            )
-            .subscribe();
-
-
-    }
-
-
-    saveCourse(changes) {
-        return fromPromise(fetch(`/api/courses/${this.course.id}`,{
-            method: 'PUT',
-            body: JSON.stringify(changes),
-            headers: {
-                'content-type': 'application/json'
-            }
-        }));
-    }
-
-
     ngAfterViewInit() {
 
-        fromEvent(this.saveButton.nativeElement, 'click')
-            .pipe(
-                exhaustMap(() => this.saveCourse(this.form.value))
-            )
-            .subscribe();
+
 
     }
 
