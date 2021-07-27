61f43ac87b09f58fabedc3724dc76cca05ff921e
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2021-07-27 10:58:23 -0500

    Use setTimeout to unsubscribe from interval after 5 seconds

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -10,16 +10,19 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    // RXJS
     const interval$ = interval(1000);
-    // interval$.subscribe(val => console.log(`interval stream 1: ${val}`));
+    const sub = interval$.subscribe(val => console.log(`stream 1: ${val}`));
+    setTimeout(() => sub.unsubscribe(), 5000);
 
     const timer$ = timer(3000, 1000);
-    // timer$.subscribe(val => console.log(`stream 1: ${val}`));
-    // timer$.subscribe(val => console.log(`stream 2: ${val}`));
 
     const click$ = fromEvent(document, 'click');
-    // click$.subscribe(val => console.log(val));
+
+    click$.subscribe(
+      evt => console.log(evt),
+      err => console.log(err),
+      () => console.log('completed')
+    );
 
     // Using built-in JavaScript api
     // document.addEventListener('click', evt => {
