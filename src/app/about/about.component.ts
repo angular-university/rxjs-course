ffbc54082fff6823ce901df3406a9361ef8abc8f
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2021-07-27 10:51:54 -0500

    Subscribe to interval and timer rxjs observables

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { interval } from 'rxjs';
+import { fromEvent, interval, timer } from 'rxjs';
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
@@ -10,8 +10,16 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-
+    // RXJS
     const interval$ = interval(1000);
+    // interval$.subscribe(val => console.log(`interval stream 1: ${val}`));
+
+    const timer$ = timer(3000, 1000);
+    // timer$.subscribe(val => console.log(`stream 1: ${val}`));
+    // timer$.subscribe(val => console.log(`stream 2: ${val}`));
+
+    const click$ = fromEvent(document, 'click');
+    // click$.subscribe(val => console.log(val));
 
     // Using built-in JavaScript api
     // document.addEventListener('click', evt => {
