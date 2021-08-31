af18e1f489dc6d248f2e7d1952c6245ae0055762
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-08-31 01:00:45 -0700

    13. Sharing HTTP Responses with the shareReplay Operator

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -26,51 +26,10 @@ export class HomeComponent implements OnInit {
 
         const courses$ = this.store.courses$;
 
-<<<<<<< Updated upstream
         this.beginnerCourses$ = this.store.selectBeginnerCourses();
 
         this.advancedCourses$ = this.store.selectAdvancedCourses();
-=======
-    const courses$: Observable<Course[]> = http$
-      .pipe(
-        // tap operatot that is meant ot  be used to produce
-        // side effects in our observable schema
-        // we want to update somethingthing outside of the observable shein
-        // such as updating a variable at the level of the component
-        // or issue a login statement
-        tap( () => console.log('http request executed')),
-        map(res => Object.values(res['payload'])),
-        // SHAREREPLAY() operator is going to make sure
-        //  that our http response is going to be passed to each subscription
-        shareReplay()
-      );
-
-    courses$.subscribe();
-    
-    this.beginnerCourses$ = courses$
-      .pipe(
-        map(courses => courses
-          .filter( course => course.category === 'BEGINNER'))
-      );
->>>>>>> Stashed changes
 
     }
 
 }
-<<<<<<< Updated upstream
-=======
-
-/**
- * this.beginnerCourses$ and this.advancedCourses$ subscriptions
- * is going to trigger the execution of a separate http request
- * because each subscription is taking over the blueprint of the stream
- * that we have here  courses$
-      .pipe(
-        map(courses => courses
-          .filter( course => course.category === 'BEGINNER'))
-      );
-  * and it is using to instatiate the concreame stream of values
-   *  go to network tab click on xhr network reques /courses will be pulled twice
-
-*/
->>>>>>> Stashed changes
