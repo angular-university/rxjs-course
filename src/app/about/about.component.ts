04026eb9c21d1154992de25883b43abd61ee32fd
JoseMMorales <josemulamorales33@hotmail.com>
2022-01-04 13:45:09 +0100

    Understanding of Stream and observable with RxJs Operators timer, interval, fromEvent

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { fromEvent, interval, Observable, timer } from "rxjs";
 
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    const interval$: Observable<number> = interval(1000);
 
+    // interval$.subscribe((val) => console.log("streamInterval 1 => " + val));
+    // interval$.subscribe((val) => console.log("streamInterval 2 => " + val));
+
+    const timer$: Observable<number> = timer(3000, 1000);
+
+    timer$.subscribe((val) => console.log("streamTimer 1 => " + val));
+
+    const click$: Observable<Event> = fromEvent(document, "click");
+    click$.subscribe((evt) => console.log(evt));
+  }
 }
