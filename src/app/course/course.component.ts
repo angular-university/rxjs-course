e467a4c998b5ec0156eb42e60a16d54e81ee7e05
JoseMMorales <josemulamorales33@hotmail.com>
2022-01-31 18:19:09 +0100

    ForkJoin operator RxJs

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -20,8 +20,9 @@ import {
   concatAll,
   shareReplay,
   throttle,
+  throttleTime,
 } from "rxjs/operators";
-import { merge, fromEvent, Observable, concat, interval } from "rxjs";
+import { merge, fromEvent, Observable, concat, interval, forkJoin } from "rxjs";
 import { Lesson } from "../model/lesson";
 import { createHttpObservable } from "../common/util";
 import { debug, RxJsLoggingLevel, setRxJsLoggingLevel } from "../common/debug";
@@ -43,25 +44,27 @@ export class CourseComponent implements OnInit, AfterViewInit {
   ngOnInit() {
     this.courseId = this.route.snapshot.params["id"];
 
-    this.course$ = createHttpObservable(`/api/courses/${this.courseId}`).pipe(
-      debug(RxJsLoggingLevel.INFO, "Course value")
-    );
+    const course$ = createHttpObservable(`/api/courses/${this.courseId}`);
 
-    setRxJsLoggingLevel(RxJsLoggingLevel.TRACE);
+    const lessons$ = this.loadLessons();
+
+    forkJoin([course$, lessons$])
+      .pipe(
+        tap(([course, lessons]) => {
+          console.log("course", course);
+          console.log("lesson", lessons);
+        })
+      )
+      .subscribe();
   }
 
   ngAfterViewInit() {
-    const searchLessons$ = fromEvent(this.input.nativeElement, "keyup").pipe(
-      map((event: any) => event.target.value),
-      startWith(""),
-      debug(RxJsLoggingLevel.TRACE, "search"),
-      debounceTime(400), //Debounce just make search once the value in input is stable (400 miliseconds in this case)
-      distinctUntilChanged(),
-      switchMap((search) => this.loadLessons(search)),
-      debug(RxJsLoggingLevel.DEBUG, "Course value")
-    );
-    const initialLessons$ = this.loadLessons();
-    this.lessons$ = concat(initialLessons$, searchLessons$);
+    const searchLessons$ = fromEvent(this.input.nativeElement, "keyup")
+      .pipe(
+        map((event: any) => event.target.value),
+        throttleTime(500)
+      )
+      .subscribe(console.log);
   }
 
   loadLessons(search = ""): Observable<Lesson[]> {
