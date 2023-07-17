a65fa1cc851abc59b80119967dddec012cfba125
Frederick Javalera <conscientiadivinus@gmail.com>
2023-07-17 15:13:05 -0600

    Commit

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Observable, fromEvent, timer } from "rxjs";
 
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    // document.addEventListener("click", (e) => {
+    //   console.log(e);
 
+    //   setTimeout(() => {
+    //     console.log("finished...");
+
+    //     let counter = 0;
+
+    //     setInterval(() => {
+    //       console.log(counter++);
+    //     }, 1000);
+    //   }, 3000);
+    // });
+
+    const interval$: Observable<number> = timer(3000, 1000);
+    const subscription = interval$.subscribe(val => console.log("stream 1 => " + val));
+
+    setTimeout(() => {
+      subscription.unsubscribe();
+    }, 5000);
+
+    const click$ = fromEvent(document, 'click');
+    click$.subscribe(
+      // Handle emitted values
+      event => console.log(event),
+      // Handle errors or...
+      error => console.log(error),
+      // ... complete.
+      () => console.log('completed')
+    );
+
+  }
 }
